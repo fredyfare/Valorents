92191da01bee99f04930b9e7ea8ad8544838a5ca
fredyfare <fredyfare@gmail.com>
2023-11-24 22:59:29 -0600

    Array json and show agents on screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-function App() {
+import agents from "./data/agents.json";
 
+function App() {
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      {agents.map((agent) => (
+        <div key={agent.number}>
+          <h1>{agent.number}</h1>
+          <h2>{agent.name}</h2>
+          <h2>{agent.role}</h2>
+          <p>{agent.biography}</p>
+          <p>{agent.released}</p>
+          <p>{agent.origin}</p>
+
+          <ul>
+            {agent.abilities.map((ability, index) => (
+              <li key={index}>
+                <p>{ability.name}</p>
+                <p>{ability.description}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
