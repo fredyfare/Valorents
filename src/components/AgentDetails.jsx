da6be312596bb6e68a5bbd3311259e2ceffba090
fredyfare <fredyfare@gmail.com>
2023-12-08 13:10:50 -0600

    Ability info shown based on selected ability icon

--- src/components/AgentDetails.jsx
+++ src/components/AgentDetails.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import valorantAgents from "../assets/images/valorantAgents.png";
 import "../styles/AgentDetails.css";
 
 function AgentDetails({ agentDetails }) {
+  const [currAbility, setCurrAbility] = useState(null);
+
+  const handleClick = (abilityKey) => {
+    setCurrAbility(abilityKey);
+  };
+
   if (!agentDetails) {
     return (
       <div className="valorant-agents">
@@ -34,21 +40,39 @@ function AgentDetails({ agentDetails }) {
       <div className="skills">
         <div className="skills-icons">
           {agentDetails.abilities.map((ability, index) => (
-            <div className="skill-icon-container" key={index}>
-              <img className="skill-icon" src="https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt29aa6cc80f1caa7b/5eaf8607af7e315106b47daa/TX_Brimstone_Q.png" />
+            <div
+              className="skill-icon-container"
+              key={index}
+              onClick={() => handleClick(ability.key)}
+            >
+              <img
+                className="skill-icon"
+                src="https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt29aa6cc80f1caa7b/5eaf8607af7e315106b47daa/TX_Brimstone_Q.png"
+              />
             </div>
           ))}
         </div>
-        <ul>
-          {agentDetails.abilities.map((ability, index) => (
-            <li key={index}>
-              <strong>
-                {ability.name} ({ability.key}):
-              </strong>{" "}
-              {ability.description}
-            </li>
-          ))}
-        </ul>
+        <div className="skill-info-container">
+          {(!currAbility
+            ? agentDetails.abilities.find((ability) => ability.key === "Q")
+            : agentDetails.abilities.find(
+                (ability) => ability.key === currAbility
+              )) &&
+            agentDetails.abilities
+              .filter((ability) =>
+                !currAbility ? ability.key === "Q" : ability.key === currAbility
+              )
+              .map((ability, index) => (
+                <div className="skill-info" key={index}>
+                  <h2 className="skill-name">
+                    {ability.key} - {ability.name}
+                  </h2>
+                  <br />
+                  <br />
+                  <p>{ability.description}</p>
+                </div>
+              ))}
+        </div>
       </div>
       <div className="agent-image">
         <h1>{agentDetails.name}</h1>
