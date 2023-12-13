40dc11db366f50e27d70250fd6b40f23c13cea7f
fredyfare <fredyfare@gmail.com>
2023-12-13 16:03:29 -0600

    Clicking the logo shows valorant agents img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../App";
 import vLogo from "../assets/images/vLogo.png";
 import "../styles/Header.css";
 
 function Header() {
+  const { agentDetails, setAgentDetails } = useContext(AppContext);
+
+  const handleClick = () => {
+    setAgentDetails(null);
+  };
+
   return (
     <nav className="header">
-      <span>
-        <img src={vLogo} className="logo" />
-      </span>
-      <p className="header-content">ALORENTS</p>
+      <div className="logo-container" onClick={handleClick}>
+        <span>
+          <img src={vLogo} className="logo" />
+        </span>
+        <p className="header-content">ALORENTS</p>
+      </div>
     </nav>
   );
 }
